69cb292f779c6be4dd51fa9107c4f109acb5e999
nkeirn <70721761+nkeirn@users.noreply.github.com>
2020-09-24 15:54:56 -0400

    Add files via upload
    I had some trouble getting the name to appear but I believe my code is close with the exception of a thing or two missing but if there's something I can do to improve it just let me know! Thank you :)

---
+++ Tech Project 3A/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tech_Project_3A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name.");
+            Console.ReadLine();
+            
+
+            try
+            {
+                string input = Console.ReadLine();
+                Console.WriteLine("hello" + input);
+                
+                
+            }
+            catch
+            {
+                Console.WriteLine("Please enter your name again.");
+                Console.WriteLine("Press any key to exit the program and try again...");
+
+            }
+        }
+    }
+}
